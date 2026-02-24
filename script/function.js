174afd23a01e58b0bcb253411cd4100ca5a6b545
hamzahassan0923 <hamzahassan0923@gmail.com>
2026-02-24 22:54:26 +0600

    added filtered btn and cards sections

---
+++ script/function.js
@@ -0,0 +1,11 @@
+let interviewList =[];
+let rejectedList =[];
+let currentStatus = 'all'
+
+let total = document.getElementById("total");
+let interview = document.getElementById("interview-count");
+let rejected = document.getElementById("rejected-count");
+
+const allFilterBtn = document.getElementById('all-filter-btn')
+const interviewFilterBtn = document.getElementById('interview-filter-btn')
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn')
